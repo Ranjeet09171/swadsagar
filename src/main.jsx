3447c2faac7cad83ecd0961d20314ad16e5da1bd
Ranjeet09171 <ranjeetbumkar843@gmail.com>
2026-03-20 15:48:41 +0530

    first commint

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 import { Provider } from 'react-redux'
-import { store } from './Store'   // ✅ Correct path
+import { store } from './store'   // ✅ Correct path
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
